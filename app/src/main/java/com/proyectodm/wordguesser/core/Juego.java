9c523090a3f69dad7bcab200a53b3d02d0cfa3e7
USUARIO <mauvanguita@gmail.com>
2022-11-09 17:43:49 +0100

    proyecto dm

---
+++ app/src/main/java/com/proyectodm/wordguesser/core/Juego.java
@@ -0,0 +1,4 @@
+package com.proyectodm.wordguesser.core;
+
+public class Juego {
+}
---
+++ app/src/main/java/com/proyectodm/wordguesser/iu/MainActivity.java
@@ -0,0 +1,16 @@
+package com.proyectodm.wordguesser.iu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.proyectodm.wordguesser.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
