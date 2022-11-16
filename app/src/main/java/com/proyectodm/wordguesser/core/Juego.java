920ba145ef54858ae4ca49842e61bd78b6c48663
USUARIO <mauvanguita@gmail.com>
2022-11-16 10:08:32 +0100

    Logica de negocio basica

--- app/src/main/java/com/proyectodm/wordguesser/core/Juego.java
+++ app/src/main/java/com/proyectodm/wordguesser/core/Juego.java
@@ -1,5 +1,61 @@
 package com.proyectodm.wordguesser.core;
 
 public class Juego {
-    int prueba;
+    int intentos=0;
+    String palabraGanadora;
+    String palabraUsuario;
+
+    int getIntentos(){
+        return intentos;
+    }
+
+    String getPalabraGanadora(){
+        return palabraGanadora;
+    }
+
+    String getPalabraUsuario(){
+        return palabraUsuario;
+    }
+
+    //Comprueba si un caracter de la palabra introducida por el usuario es igual al de la palabra ganadora
+    public String comprobarCaracter(String palabra, String palabraCorrecta, int n) {
+
+        char caracter1= Character.toUpperCase(palabraGanadora.charAt(n));
+        char caracter2= Character.toUpperCase(palabraUsuario.charAt(n));
+
+        if(caracter2 == caracter1){
+            return "verde";
+        }
+
+        else if(palabraCorrecta.indexOf(caracter2)!=-1)
+            return "amarillo";
+
+        return "gris";
+
+    }
+
+    //metodo que comprueba si la palabra que mete el usuario esta registrada en el diccionario
+    public boolean comprobarPalabraExiste(String palabra)  {
+
+        //String[] igualar=this.palabras;
+//10836 seria el supuesto numero de palabras, pero eso lo obtenemos con un array.lenght
+
+        for (int i = 0; i < 10836; i++) {
+/*
+            if (igualar[i].compareToIgnoreCase(palabraUsuario)==0) {
+                return true;
+                */
+        }
+        System.out.println("La palabra introducida ha de existir en el diccionario");
+        return false;
+    }
+
+    //metodo para contar los intentos que lleva el jugador
+    public boolean seguirJugando(int intentos) {
+        if (intentos < 5) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/proyectodm/wordguesser/core/Jugador.java
+++ app/src/main/java/com/proyectodm/wordguesser/core/Jugador.java
@@ -1,4 +1,72 @@
 package com.proyectodm.wordguesser.core;
 
 public class Jugador {
+
+    //variables que considero importantes en esta clase
+    int partidas = 0;
+    int victorias = 0;
+    int perdidas = 0;
+
+    //crear una clase persona y que jugador herede de ella?
+    String nombre;
+    String apellidos;
+    String usuario;
+    String contraseña;
+
+    //getters
+
+    int getPartidas(){
+        return partidas;
+    }
+
+    int getVictorias(){
+        return victorias;
+    }
+
+    int getPerdidas(){
+        return perdidas;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    String getApellidos(){
+        return apellidos;
+    }
+
+    String getUsuario(){
+        return usuario;
+    }
+
+    String getContraseña(){
+        return contraseña;
+    }
+
+    //setters
+
+    void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+
+    void setUsuario(String usuario){
+        this.usuario = usuario;
+    }
+
+    //metodo que devuelve la mejor racha
+    int calcularMejorRacha(){
+        int racha=0;
+        return racha;
+    }
+
+    //metodo que devuelve la racha actual
+    int calcularRachaActual(){
+        int racha=0;
+        return racha;
+    }
+
 }
