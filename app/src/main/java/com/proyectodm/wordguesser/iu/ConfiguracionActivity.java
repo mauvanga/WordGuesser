d8e01739a2cd77e43c4683a481714e8d848aaa35
Cibranix <cccabaleiro19@esei.uvigo.es>
2022-11-27 14:15:22 +0100

    borrar usuario listo

--- app/src/main/java/com/proyectodm/wordguesser/iu/ConfiguracionActivity.java
+++ app/src/main/java/com/proyectodm/wordguesser/iu/ConfiguracionActivity.java
@@ -1,6 +1,13 @@
 package com.proyectodm.wordguesser.iu;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.proyectodm.wordguesser.R;
@@ -10,6 +17,66 @@ public class ConfiguracionActivity extends WordGuesserActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuracion); // Cargar la del modo correspondiente
+        setContentView(R.layout.activity_configuracion);
+
+        TextView textViewName = findViewById(R.id.textViewCurrentPlayerName);
+        TextView textViewSurname = findViewById(R.id.textViewCurrentPlayerSurname);
+        TextView textViewUsername = findViewById(R.id.textViewCurrentPlayerUsername);
+
+        textViewName.setText(getJugadorLogueado().getNombre());
+        textViewSurname.setText(getJugadorLogueado().getApellidos());
+        textViewUsername.setText(getJugadorLogueado().getUsuario());
+
+        Button buttonModifyData = findViewById(R.id.buttonModificarDatos);
+        Button buttonChangePassword = findViewById(R.id.buttonCambiarPass);
+        Button buttonDeletePlayer = findViewById(R.id.buttonEliminarCuenta);
+
+        buttonModifyData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonDeletePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder seguroBorrarUsuario = new AlertDialog.Builder(ConfiguracionActivity.this);
+                seguroBorrarUsuario.setMessage("¿Seguro que quieres borrar el usuario?").setCancelable(false)
+                        .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // volvemos a la actividad MainActivity que nos llevará al LoginActivity
+                                // ya que no habrá usuario conectado
+                                Intent i = new Intent(ConfiguracionActivity.this, MainActivity.class);
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                i.putExtra("EXIT", true);
+                                getDbManager().deletePlayer(getJugadorLogueado().getIdJugador());
+                                clearJugadorLogueado();
+                                startActivity(i);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert = seguroBorrarUsuario.create();
+                // alert.setTitle("...");
+                alert.show();
+            }
+        });
     }
 }
